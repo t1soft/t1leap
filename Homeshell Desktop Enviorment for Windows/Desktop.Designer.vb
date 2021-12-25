1be1817bfcec483c3545adc8d2dbf6f578ad3331
gomas navsda <privatefarm@outlook.com>
2021-12-24 18:52:34 -1000

    Add Milestone Info

--- Homeshell Desktop Enviorment for Windows/Desktop.Designer.vb
+++ Homeshell Desktop Enviorment for Windows/Desktop.Designer.vb
@@ -83,8 +83,8 @@ Partial Class Desktop
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(174, 105)
         Me.Label1.TabIndex = 5
-        Me.Label1.Text = "Homeshell Desktop Enviorment" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "for Windows " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Milestone 2 Build 211217. " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(c) 2" &
-    "021 T1 Software All " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Rights Reserved"
+        Me.Label1.Text = "Homeshell Desktop Enviorment" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "for Windows " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Milestone 3 Build 1. " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "(c) 2021 T" &
+    "1 Software All " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Rights Reserved"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopRight
         '
         'Panel1
